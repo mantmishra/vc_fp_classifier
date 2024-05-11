d6cc2c44ea42b5d1938aeb1074d0c885438567ac
mantmishra <mantmishra@informatica.com>
2024-05-11 12:16:28 +0530

    Initial commit

---
+++ classifier.py
@@ -0,0 +1,15 @@
+# MODELS
+## Native Bayes - general purpose classifier
+## Logistic reg - 
+## SVM
+## LSTM
+## RKWV - checking RNN architecture
+## BERT
+## ALBERT
+
+
+import pytorch
+import scikit_learn
+import tensorflow 
+import spacy
+
